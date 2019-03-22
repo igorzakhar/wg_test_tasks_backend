f60d561f1fe1ca4b802f87932daa871d99909cfd
igorzakhar <igorzakhar@gmail.com>
2019-03-22 11:53:40 +0300

    Replace get_cats_stat() with get_mean_median_mode()

--- cats_task2.py
+++ cats_task2.py
@@ -4,40 +4,36 @@ import psycopg2
 DSN = 'dbname=wg_forge_db user=wg_forge password=42a host=localhost port=5432'
 
 
-def get_cats_stat(cursor):
+def get_mean_median_mode(cursor, column_name):
     query = '''
         SELECT *
         FROM
-        (SELECT ARRAY(
-        SELECT tail_length
-        FROM cats
-        GROUP BY tail_length
-        HAVING COUNT(*) >= ALL(SELECT count(*)
-        FROM cats GROUP BY tail_length)) AS tail_length_mode
-        ) AS tail_length_mode,
-        (SELECT ARRAY(
-        SELECT whiskers_length
-        FROM cats
-        GROUP BY whiskers_length
-        HAVING COUNT(*) >= ALL(SELECT count(*)
-        FROM cats GROUP BY whiskers_length)) AS twhiskers_length_mode
-        ) AS whiskers_length_mode,
         (SELECT
-        CAST(AVG(tail_length) AS DECIMAL(10,2)) AS tail_length_mean,
-        CAST(AVG(whiskers_length) AS DECIMAL(10,2)) AS whiskers_length_mean,
-        PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY tail_length)
-        AS tail_length_median,
-        PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY whiskers_length)
-        AS whiskers_length_median
+        CAST(AVG({column_name}) AS DECIMAL(10,2)) AS {column_name}_mean,
+        PERCENTILE_CONT(0.5) WITHIN GROUP(ORDER BY {column_name})
+        AS {column_name}_median,
+        (SELECT ARRAY(
+        SELECT {column_name}
+        FROM cats
+        GROUP BY {column_name}
+        HAVING COUNT(*) >= ALL(SELECT count(*)
+        FROM cats GROUP BY {column_name})) AS {column_name}_mode
+        ) AS {column_name}_mode
         FROM cats) AS stats;
     '''
-
     try:
-        cursor.execute(query)
+        cursor.execute(query.format(column_name=column_name))
     except psycopg2.Error as error:
         raise error
 
     return cursor.fetchall()
+
+
+def insert_cat_color_info(curs, records):
+    try:
+        pass
+    except psycopg2.Error as error:
+        raise error
 
 
 def main():
@@ -46,11 +42,12 @@ def main():
     try:
         with psycopg2.connect(DSN) as conn:
             with conn.cursor() as curs:
-                cats_statistic = get_cats_stat(curs)
+                tail_stat = get_mean_median_mode(curs, 'tail_length')
+                whiskers_stat = get_mean_median_mode(curs, 'whiskers_length')
     except psycopg2.Error as error:
         print(error)
     else:
-        print(cats_statistic)
+        print(tail_stat, whiskers_stat)
     finally:
         if conn is not None:
             conn.close()
